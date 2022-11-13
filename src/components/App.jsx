27af2735c4da0adff3b3eac17651df7eb813363e
Heorgii <rushchakheorgii@gmail.com>
2022-11-13 16:18:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 import css from './App.module.css'
 
-function App() {
+const App = () =>{
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
